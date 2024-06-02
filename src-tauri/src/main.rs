3f906e559b5d9bd45fea434f30744058dd6bd548
Abydos12 <38505162+Abydos12@users.noreply.github.com>
2024-06-02 23:32:35 +0200

    feat: improve token retrieval

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env::consts::OS;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+
+use directories::UserDirs;
+
 
 #[tauri::command]
 fn get_zerotier_service_token() -> Result<String, String> {
-    let mut file = File::open("C:\\Users\\Kosch\\AppData\\Local\\ZeroTier\\authtoken.secret")
+    let path = get_token_path().map_err(|e| e.to_string())?;
+    let mut file = File::open(path)
         .map_err(|e| e.to_string())?;
     let mut token = String::new();
     file.read_to_string(&mut token).map_err(|e| e.to_string())?;
     Ok(token)
+}
+
+fn get_token_path() -> Result<PathBuf, String> {
+    let home: &Path;
+    if let Some(user_dirs) = UserDirs::new() {
+        home = user_dirs.home_dir();
+        return match OS {
+            "windows" => Ok(home.join("AppData\\Local\\ZeroTier\\authtoken.secret")),
+            "linux" => Ok(Path::new("/var/lib/zerotier-one").into()),
+            "macos" => Ok(home.join("Library/Application Support/ZeroTier/authtoken.secret")),
+            _ => Err("OS not supported".into())
+        };
+    } else {
+        panic!("Home dir not found");
+    }
 }
 
 fn main() {
