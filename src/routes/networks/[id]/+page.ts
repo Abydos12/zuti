6e115d8258de262b969310ba9ee086accdac5209
Abydos12 <38505162+Abydos12@users.noreply.github.com>
2024-06-25 23:31:45 +0200

    feat: adds loading state

--- src/routes/networks/[id]/+page.ts
+++ src/routes/networks/[id]/+page.ts
@@ -2,9 +2,10 @@ import type { PageLoad } from "./$types";
 import { zerotierApi } from "$lib/zerotier/api";
 import { goto } from "$app/navigation";
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({
+  params,
+}): Promise<{ network: Network }> => {
   const network = await zerotierApi.getNetworkById(params.id);
-  console.debug({ network });
 
   if (!network) {
     await goto("/");
--- src/routes/networks/join/+page.svelte
+++ src/routes/networks/join/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { zerotierApi } from "$lib/zerotier/api";
   import { networkIdPattern } from "$lib/patterns";
+  import { goto } from "$app/navigation";
+  import { writable } from "svelte/store";
 
   const networkSettings = {
     allowManaged: true,
@@ -9,50 +11,66 @@
     allowDNS: false,
   };
 
+  const loading = writable(false);
+
   let networkId: string = "";
 
-  function joinNetwork() {
-    zerotierApi
-      .joinNetwork(networkId, networkSettings)
-      .then(console.debug)
-      .catch(console.error);
+  async function joinNetwork() {
+    loading.set(true);
+    try {
+      await zerotierApi.joinNetwork(networkId, networkSettings);
+      goto(networkId);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      loading.set(false);
+    }
   }
 </script>
 
-<form
-  class="m-4 flex flex-col items-center gap-4"
-  on:submit|preventDefault={joinNetwork}
->
-  <input
-    type="text"
-    pattern={networkIdPattern}
-    required
-    placeholder={"#".repeat(16)}
-    size="16"
-    name="networkId"
-    bind:value={networkId}
-  />
-  <button type="submit">Join</button>
+{#if $loading}
+  <div class="m-auto">Loading...</div>
+{:else}
+  <form
+    class="m-4 flex flex-col items-center gap-4"
+    on:submit|preventDefault={() => joinNetwork()}
+  >
+    <input
+      type="text"
+      pattern={networkIdPattern}
+      required
+      placeholder={"#".repeat(16)}
+      size="16"
+      name="networkId"
+      bind:value={networkId}
+    />
+    <button
+      type="submit"
+      class="rounded-sm bg-zinc-800 px-4 py-1 hover:bg-zinc-700 hover:text-white"
+    >
+      Join
+    </button>
 
-  <div class="flex flex-wrap gap-8">
-    <label class="flex items-center gap-2">
-      <input type="checkbox" bind:checked={networkSettings.allowManaged} />
-      Allow Managed
-    </label>
+    <div class="flex flex-wrap gap-8">
+      <label class="flex items-center gap-2">
+        <input type="checkbox" bind:checked={networkSettings.allowManaged} />
+        Allow Managed
+      </label>
 
-    <label class="flex items-center gap-2">
-      <input type="checkbox" bind:checked={networkSettings.allowGlobal} />
-      Allow Global
-    </label>
+      <label class="flex items-center gap-2">
+        <input type="checkbox" bind:checked={networkSettings.allowGlobal} />
+        Allow Global
+      </label>
 
-    <label class="flex items-center gap-2">
-      <input type="checkbox" bind:checked={networkSettings.allowDefault} />
-      Allow Default
-    </label>
+      <label class="flex items-center gap-2">
+        <input type="checkbox" bind:checked={networkSettings.allowDefault} />
+        Allow Default
+      </label>
 
-    <label class="flex items-center gap-2">
-      <input type="checkbox" bind:checked={networkSettings.allowDNS} />
-      Allow DNS
-    </label>
-  </div>
-</form>
+      <label class="flex items-center gap-2">
+        <input type="checkbox" bind:checked={networkSettings.allowDNS} />
+        Allow DNS
+      </label>
+    </div>
+  </form>
+{/if}
