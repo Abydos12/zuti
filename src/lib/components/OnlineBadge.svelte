03f5d8059c668fedd41ae803492114d1557ddcbf
Abydos12 <38505162+Abydos12@users.noreply.github.com>
2024-07-01 22:59:09 +0200

    ui: change OnlineBadge

--- src/lib/components/OnlineBadge.svelte
+++ src/lib/components/OnlineBadge.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   export let online: boolean | undefined;
+
+  $: statusText =
+    online === true ? "ONLINE" : online === false ? "OFFLINE" : "UNKNOWN";
+
+  $: badgeClass =
+    online === true
+      ? "border-green-500 bg-green-700 text-green-100"
+      : online === false
+        ? "border-red-500 bg-red-700 text-red-100"
+        : "bg-zinc-700 border-zinc-500";
 </script>
 
-{#if online === true}
-  <code class="rounded bg-green-700 px-2 font-semibold text-green-100">
-    ONLINE
-  </code>
-{:else if online === false}
-  <code class="rounded bg-red-700 px-2 font-semibold text-red-100">
-    OFFLINE
-  </code>
-{:else}
-  <code class="rounded bg-zinc-700 px-2 font-semibold"> UNKNOWN </code>
-{/if}
+<code class="rounded border-2 bg-opacity-50 px-2 font-semibold {badgeClass}">
+  {statusText}
+</code>
