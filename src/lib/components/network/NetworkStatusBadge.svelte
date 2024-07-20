5b10446ebb7469480ef1b366dcca4f778b526cf5
Abydos12 <38505162+Abydos12@users.noreply.github.com>
2024-07-20 16:32:30 +0200

    feat: improves ip / ports display

--- src/lib/components/network/NetworkStatusBadge.svelte
+++ src/lib/components/network/NetworkStatusBadge.svelte
@@ -2,12 +2,19 @@
   import type { Network } from "$lib/zerotier/models";
 
   export let status: Network["status"];
+
+  $: badgeClass = switchClass(status);
+
+  function switchClass(s: Network["status"]): string {
+    switch (s) {
+      case "OK":
+        return "bg-green-800/25 text-green-500 border-green-800";
+      default:
+        return "bg-red-800/25 text-red-500 border-red-800";
+    }
+  }
 </script>
 
-<code
-  class="rounded bg-zinc-800 px-2 font-semibold"
-  class:text-green-500={status === "OK"}
-  class:text-red-500={status !== "OK"}
->
+<code class="rounded-sm border px-2 font-semibold {badgeClass}">
   {status}
 </code>
